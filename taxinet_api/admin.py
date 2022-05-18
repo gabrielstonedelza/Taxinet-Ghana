05a480d1b8beff98db8543044eef2c5954044b2b
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-05-18 15:43:45 +0000

    taxinet,querying for just drivers and passengers

--- taxinet_api/admin.py
+++ taxinet_api/admin.py
@@ -4,30 +4,10 @@ from .models import (RequestRide, BidRide, ScheduleRide, BidScheduleRide, Notifi
                      DriversLocation, DriversPoints, ConfirmDriverPayment,SearchedDestinations)
 
 
-class RideRequestAdmin(admin.ModelAdmin):
-    list_display = (
-        'id', 'driver', 'passenger', 'pick_up', 'drop_off', 'ride_accepted', 'ride_rejected', 'price', 'completed',
-        'driver_booked', 'date_requested', 'passengers_pick_up_place_id','passengers_drop_off_place_id')
-
-
-class BidRideAdmin(admin.ModelAdmin):
-    list_display = ('id', 'ride', 'user', 'bid', 'date_accepted')
-
-
-class BidScheduleRideAdmin(admin.ModelAdmin):
-    list_display = ('id', 'scheduled_ride', 'user', 'bid', 'date_accepted')
-
-
-class ScheduleRideAdmin(admin.ModelAdmin):
-    list_display = (
-        'id', 'passenger', 'driver', 'date_of_pickup', 'time_of_pickup', 'schedule_option', 'pickup_location',
-        'drop_off_location',  'scheduled', 'price', 'initial_payment', 'date_scheduled')
-
-
-admin.site.register(RequestRide, RideRequestAdmin)
-admin.site.register(BidRide, BidRideAdmin)
-admin.site.register(ScheduleRide, ScheduleRideAdmin)
-admin.site.register(BidScheduleRide, BidScheduleRideAdmin)
+admin.site.register(RequestRide)
+admin.site.register(BidRide)
+admin.site.register(ScheduleRide)
+admin.site.register(BidScheduleRide)
 admin.site.register(Notifications)
 admin.site.register(Complains)
 admin.site.register(DriverReviews)
