59d4cf6bfe95ca2e1fb0a5d54834e11fbf965723
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-05-27 15:31:30 +0000

    taxinet,created new model for accept and reject bid

--- taxinet_api/views.py
+++ taxinet_api/views.py
@@ -167,11 +167,11 @@ def get_all_bids(request, ride_id):
     return Response(serializer.data)
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 @permission_classes([permissions.IsAuthenticated])
 def post_bid_accept_or_reject(request, bid_id, ride_id):
-    ride = get_object_or_404(RequestRide, id=ride_id)
     bid = get_object_or_404(BidRide, id=bid_id)
+    ride = get_object_or_404(RequestRide, id=ride_id)
     serializer = AcceptRejectBidSerializer(bid, ride, data=request.data)
     if serializer.is_valid():
         serializer.save(user=request.user)
