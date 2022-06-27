e677a2d9fb9c7b8c0e9d7297fbb4bf895d2a58c4
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-06-27 10:17:57 +0000

    taxinet api,removed custom admin model

--- taxinet_users/admin.py
+++ taxinet_users/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from .models import DriverProfile, PassengerProfile, User
 
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'user_type', 'full_name', 'phone_number',)
-
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
 admin.site.register(DriverProfile)
 admin.site.register(PassengerProfile)
