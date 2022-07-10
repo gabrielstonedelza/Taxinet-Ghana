d7fa51894f6e4b2fb1a9483d072834019dc1851e
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-07-10 15:04:54 +0000

    taxinet api,added administrator and investor profiles

--- taxinet_users/admin.py
+++ taxinet_users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import DriverProfile, PassengerProfile, User
+from .models import DriverProfile, PassengerProfile, User, AdministratorsProfile, InvestorsProfile
 
 admin.site.register(User)
 admin.site.register(DriverProfile)
 admin.site.register(PassengerProfile)
+admin.site.register(AdministratorsProfile)
+admin.site.register(InvestorsProfile)
