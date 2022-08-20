ebecdafcb86f0c8d4a522dfba29577a3e7c8baac
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-08-20 15:54:11 +0000

    taxinet api,load wallet

--- taxinet_api/serializers.py
+++ taxinet_api/serializers.py
@@ -222,7 +222,7 @@ class DriverVehicleInventorySerializer(serializers.ModelSerializer):
                   'tail_rear_lights', 'reverse_lights', 'interior_lights', 'engine_noise', 'excessive_smoke',
                   'foot_break', 'hand_break', 'wheel_bearing_noise', 'warning_triangle', 'fire_extinguisher',
                   'first_aid_box', 'checked_today', 'date_checked', 'time_checked', 'get_drivers_name',
-                  'get_driver_profile_pic']
+                  'get_driver_profile_pic','read']
         read_only_fields = ['driver']
 
     def get_username(self, user):
