a732cb468e3c0bcdbff847510a01025311b992f6
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-08-23 09:11:31 +0000

    taxinet api,getting specific users wallets

--- taxinet_api/views.py
+++ taxinet_api/views.py
@@ -739,6 +739,6 @@ def get_all_driver_payments(request):
 @api_view(['GET'])
 @permission_classes([permissions.AllowAny])
 def get_my_wallet(request):
-    wallet = PassengersWallet.objects.filter(passenger=request.user.id)
+    wallet = PassengersWallet.objects.filter(passenger=request.user.id).order_by('-date_loaded')
     serializer = PassengerWalletSerializer(wallet, many=True)
     return Response(serializer.data)
