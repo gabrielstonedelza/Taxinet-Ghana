78722b8d388dcbd39512bf497d8a2302785dcf1d
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-09-01 18:02:14 +0000

    taxinet api,removed package timeloop

--- taxinet_api/models.py
+++ taxinet_api/models.py
@@ -13,30 +13,8 @@ DeUser = settings.AUTH_USER_MODEL
 
 #
 
-import time
-from timeloop import Timeloop
+
 from datetime import datetime, date, time, timedelta
-
-tl = Timeloop()
-
-
-@tl.job(interval=timedelta(seconds=2))
-def sample_job_every_2s():
-    print("2s job current time : {}".format(time.ctime()))
-
-
-@tl.job(interval=timedelta(seconds=5))
-def sample_job_every_5s():
-    print("5s job current time : {}".format(time.ctime()))
-
-
-@tl.job(interval=timedelta(seconds=10))
-def sample_job_every_10s():
-    print("10s job current time : {}".format(time.ctime()))
-
-
-if __name__ == "__main__":
-    tl.start(block=True)
 # Create your models here.
 READ_STATUS = (
     ("Read", "Read"),
