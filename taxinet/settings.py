90fc00f6f303c8e58aa7ff6df739fd6f321f7ed5
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-09-11 21:50:18 +0000

    taxinet api,adding unique codes to passengers profile models

--- taxinet/settings.py
+++ taxinet/settings.py
@@ -163,7 +163,7 @@ DJOSER = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
