6d1a50fbdedf5293004b0009a025940319f8199f
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-09-16 17:50:00 +0000

    taxinet api,changed ride message user to driver and passenger

--- taxinet_api/serializers.py
+++ taxinet_api/serializers.py
@@ -351,5 +351,4 @@ class UpdatedWalletsSerializer(serializers.ModelSerializer):
 class RideMessagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = RideMessages
-        fields = ['id', 'ride', 'user', 'message', 'read', 'date_sent', 'time_sent', 'get_profile_pic', 'get_username',
-                  'get_user_type']
+        fields = ['id', 'ride', 'driver', 'passenger', 'message', 'read', 'date_sent', 'time_sent', 'get_profile_pic', 'get_driver_username', 'get_passenger_username']
