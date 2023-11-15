d452b2f7441cdfa07190e9753de50f6bb5dafaa4
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-15 14:03:18 +0000

    taxinet api,added models and function to add approvals

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("book_flight/",views.book_flight),
-    path("get_my_flights/", views.get_my_flight_requests),
-    path("get_all_flights/", views.get_all_flight_requests),
+    path("get_my_flight_requests/", views.get_my_flight_requests),
+    path("get_all_flight_requests/", views.get_all_flight_requests),
     path("delete_flight/<int:id>/",views.delete_flight),
     path("add_to_booked/<int:id>/",views.add_to_booked),
     path("get_my_booked_flights/",views.get_my_booked_flights),
