b3053789bbcaa18f2d1ad59e50e32a3da0e03e3c
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-17 16:42:55 +0000

    taxinet api,added admin models

--- car_sales/admin.py
+++ car_sales/admin.py
@@ -2,8 +2,25 @@ from django.contrib import admin
 
 from .models import Vehicle, AddCarImage, BuyVehicle, AddToApprovedVehiclePurchases
 
+class AdminBuyVehicleModel(admin.ModelAdmin):
+    list_display = ['id','user','vehicle','request_approved','date_requested']
+
+    class Meta:
+        model = BuyVehicle
+
+class AdminAddToApprovedVehiclePurchases(admin.ModelAdmin):
+    list_display = ['id','user','vehicle','date_approved']
+
+    class Meta:
+        model = AddToApprovedVehiclePurchases
+
+
+class AdminVehicleModel(admin.ModelAdmin):
+    list_display = "__all__"
+    class Meta:
+        model = Vehicle
 
 admin.site.register(Vehicle)
 admin.site.register(AddCarImage)
-admin.site.register(BuyVehicle)
-admin.site.register(AddToApprovedVehiclePurchases)
+admin.site.register(BuyVehicle,AdminBuyVehicleModel)
+admin.site.register(AddToApprovedVehiclePurchases,AdminAddToApprovedVehiclePurchases)
