85af1f0c38f92615f5dd34f19fe39285fee88564
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-17 16:07:32 +0000

    taxinet api,updating user wallet

--- schedule/apps.py
+++ schedule/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ScheduleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'schedule'
+
+    def ready(self):
+        import schedule.signals
