fd35d8f9b3160c17b5057ea35fab37f037a6a0d7
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-17 09:19:04 +0000

    taxinet api,added car as a foreign key to drive and pay model

---
+++ wallets/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from notifications.models import Notifications
+from .models import UpdatedWallets
+from users.models import User
+
+
+@receiver(post_save,sender=UpdatedWallets)
+def alert_booking_request(sender,created,instance,**kwargs):
+    if created:
+        admin_user = User.objects.get(id=1)
+        title = "Wallet Updated"
+        message = f"Hi {instance.wallet.user.username} ,your wallet was updated"
+
+        Notifications.objects.create(item_id=instance.id,notification_title=title,notification_message=message,notification_to=instance.wallet.user,notification_from=admin_user)
