d87f5889feb646472983fc6d3902938da8851850
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-19 11:37:42 +0000

    taxinet api,added drive type to car model

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("profile/",views.get_my_profile),
-    path("profile/<int:id>/update/",views.update_profile),
+    path("profile/update/",views.update_profile),
 ]
