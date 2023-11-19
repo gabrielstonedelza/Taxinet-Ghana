ab0c8aedb48ea1573dfea4022f3db71071c3ab78
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-19 12:14:21 +0000

    taxinet api,added drive type to car model

--- ticketing/serializers.py
+++ ticketing/serializers.py
@@ -4,7 +4,7 @@ from .models import Booking, AvailableFlights,RequestBooking
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = ['id','user','flight','flight_booked','date_booked','get_username','get_user_phone','flight_duration','adults','infants','get_airline','get_depart_airport','get_arrival_airport','get_flight_type','get_depart_date','get_flight_duration','get_depart_time','get_price','get_return_date','get_return_time','get_arrival_time']
+        fields = ['id','user','flight','flight_booked','date_booked','get_username','get_user_phone','adults','infants','get_airline','get_depart_airport','get_arrival_airport','get_flight_type','get_depart_date','get_flight_duration','get_depart_time','get_price','get_return_date','get_return_time','get_arrival_time']
         read_only_fields = ['flight']
 
 
@@ -16,5 +16,5 @@ class AvailableFlightsSerializer(serializers.ModelSerializer):
 class RequestBookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = RequestBooking
-        fields = ['id','user','flight','flight_booked','date_booked','get_username','get_user_phone','flight_duration','adults','infants','get_airline','get_depart_airport','get_arrival_airport','get_flight_type','get_depart_date','get_flight_duration','get_depart_time','get_price','get_return_date','get_return_time','get_arrival_time']
+        fields = ['id','user','flight','flight_booked','date_booked','get_username','get_user_phone','adults','infants','get_airline','get_depart_airport','get_arrival_airport','get_flight_type','get_depart_date','get_flight_duration','get_depart_time','get_price','get_return_date','get_return_time','get_arrival_time']
         read_only_fields = ['flight']
