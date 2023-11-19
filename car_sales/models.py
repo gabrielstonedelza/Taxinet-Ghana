e8b2e88cc350b8fa301179819e92d4740edeed15
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-19 08:23:10 +0000

    taxinet api,added drive type to car model

--- car_sales/models.py
+++ car_sales/models.py
@@ -43,9 +43,15 @@ RENT_TYPE = (
     ("Pay And Drive","Pay And Drive"),
     ("Drive And Pay","Drive And Pay"),
 )
+
+DRIVE_TYPE = (
+    ("Self Drive","Self Drive"),
+    ("With Driver","With Driver"),
+)
 class Vehicle(models.Model):
     purpose = models.CharField(max_length=50,choices=VEHICLE_PURPOSE, default="For Sale")
-    rent_type = models.CharField(max_length=50,choices=RENT_TYPE, default="Pay And Drive")
+    rent_type = models.CharField(max_length=50,choices=RENT_TYPE, default="With Driver")
+    drive_type = models.CharField(max_length=50,choices=DRIVE_TYPE, default="Pay And Drive")
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=19, decimal_places=2, default=0.0)
     location = models.CharField(max_length=100)
