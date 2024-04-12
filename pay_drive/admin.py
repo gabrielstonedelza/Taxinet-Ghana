a17dacffbc1c774e9e855a898d7895697cef512d
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2024-04-12 08:47:54 -0500

    taxinet api,added new models for cars for rent,edited wallets and referrals

--- pay_drive/admin.py
+++ pay_drive/admin.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from .models import RequestPayAndDrive, AddToApprovedPayAndDrive, PayDailyPayAndDrive
 
 class AdminRequestPayAndDrive(admin.ModelAdmin):
-    list_display = ['id','user','car','drive_type','pick_up_date','period_total_price','request_approved','date_requested']
+    list_display = ['id','user','car','drive_type','pick_up_date','period_total_price','request_approved','date_requested','referral']
     class Meta:
         model = RequestPayAndDrive
 
 class AdminAddToApprovedPayAndDrive(admin.ModelAdmin):
-    list_display = ['id','pay_and_drive','user','assigned_driver','date_approved']
+    list_display = ['id','pay_and_drive','user','assigned_driver','date_approved','expired','dropped_off']
     class Meta:
         model = AddToApprovedPayAndDrive
 
