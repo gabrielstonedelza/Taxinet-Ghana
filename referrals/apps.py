9b778ce4998b3cdc93be2a8cb100bdc41cbcc062
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2024-04-14 18:58:07 -0500

    taxinet api,removed signals from referrals

--- referrals/apps.py
+++ referrals/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class ReferralsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'referrals'
-
-    def ready(self):
-        import referrals.signals
--- referrals/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import ReferralWallets
-
-
-@receiver(post_save, sender=ReferralWallets)
-def create_referral_wallets(sender, created, instance, **kwargs):
-    if created:
-        ReferralWallets.objects.create(referral=instance)
