948e3d614ac98f22b5a66ed11105afd9d3a0655e
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2024-05-19 11:37:13 -0500

    taxinet api,added kilometers prices to car rental model

--- cars_for_rent/models.py
+++ cars_for_rent/models.py
@@ -5,15 +5,30 @@ CAR_TRANSMISSIONS = (
     ("Manual", "Manual"),
 )
 
+DRIVE_TYPE = (
+    ("Self Drive", "Self Drive"),
+    ("With a Driver", "With a Driver"),
+)
+
 class CarsForRent(models.Model):
     name = models.CharField(max_length=100)
     engine_type = models.CharField(max_length=10)
     seater = models.IntegerField(default=5)
     car_model = models.CharField(max_length=100,default="")
     transmission = models.CharField(max_length=100,default="Automatic")
+    drive_type = models.CharField(max_length=50,default="With a Driver", choices=DRIVE_TYPE)
     color = models.CharField(max_length=100,default="Black")
     picture = models.ImageField(upload_to="Cars_for_rent")
+    outside_ksi = models.BooleanField(default=False)
+    k200 = models.CharField(max_length=20,default="Ghc900")
+    k300 = models.CharField(max_length=20,default="Ghc1000")
+    k400 = models.CharField(max_length=20,default="Ghc1200")
+    k500 = models.CharField(max_length=20,default="Ghc1300")
+    k600 = models.CharField(max_length=20,default="Ghc1400")
     description = models.TextField(default="")
+    # inside of ksi with a driver
+    kk200 = models.CharField(max_length=20,default="Ghc700")
+
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- cars_for_rent/serializers.py
+++ cars_for_rent/serializers.py
@@ -4,8 +4,7 @@ from .models import CarsForRent, AddCarImage
 class CarsForRentSerializer(serializers.ModelSerializer):
     class Meta:
         model = CarsForRent
-        fields = ['id','name','engine_type','seater','transmission','car_model','picture','date_added','get_car_pic','description','color']
-
+        fields = ['id','name','engine_type','seater','transmission','car_model','picture','date_added','get_car_pic','description','color','drive_type','outside_ksi','k200','k300','k400','k500','k600','kk200']
 
 class AddCarImageSerializer(serializers.ModelSerializer):
     class Meta:
